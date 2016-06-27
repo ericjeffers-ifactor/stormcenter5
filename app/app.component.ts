bb90c845e00030e98dab0faf1cd5bb09f49930bc
eric.jeffers@ifactorinc.com <eric.jeffers@ifactorinc.com>
2016-06-27 16:01:21 -0700

    Merging angular quickstart repo.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,13 @@
 import {Component} from '@angular/core';
+import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES} from 'angular2-google-maps/core'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    directives: [ANGULAR2_GOOGLE_MAPS_DIRECTIVES],
+    templateUrl: './app/templates/app.template.html',
+    styleUrls: ['./app/styles/app.style.css']
 })
-export class AppComponent { }
+export class AppComponent { 
+    lat: number = 51.678418;
+  lng: number = 7.809007;
+}
